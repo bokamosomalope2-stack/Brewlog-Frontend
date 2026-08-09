5e571cdb688b5398eb3eeb8f2d33079398d66d0b
Bokamoso malope <kaahoza@gmail.com>
2026-08-10 00:27:55 +0200

    fix: update config with my exact unique render subdomain endpoint

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080/api/brews' : 'https://onrender.com';
+export const API_BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080/api/brews' : 'https://brewlog-backend-1.onrender.com';
